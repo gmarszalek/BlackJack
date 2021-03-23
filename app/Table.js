42c6ced3ea32ee488a08aa7be160d0e2cacc4c60
gmarszalek <grzegorzmarszalek92@gmail.com>
2021-03-23 20:11:00 +0100

    final commit

---
+++ app/Table.js
@@ -0,0 +1,13 @@
+export class Table {
+  constructor(playersCards, dealersCards) {
+    this.dealersCards = dealersCards;
+    this.playersCards = playersCards;
+  }
+
+  showPlayersCard(card) {
+    this.playersCards.appendChild(card.render());
+  }
+  showDealersCard(card) {
+    this.dealersCards.appendChild(card.render());
+  }
+}
